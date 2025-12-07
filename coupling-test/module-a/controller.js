02a78742f7401c2ffd23f5238e8c17378a5cf8a2
Reistoge <64452194+Reistoge@users.noreply.github.com>
2025-12-07 17:46:17 -0300

    test

--- coupling-test/module-a/controller.js
+++ coupling-test/module-a/controller.js
@@ -4,6 +4,15 @@ import { UserService, startService } from '../module-b/service.js';
 // Functions outside class
 export function initApp() {
   startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
+  startService(); // Function Coupling: Calls service.js
 }
 
 export function shutdownApp() {
